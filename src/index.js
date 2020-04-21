627f6f59cc8b7376de804f3ca1b9a078aee353b8
lluisemper <lluisemper@gmail.com>
2020-04-21 22:20:05 +0200

    eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 import List from './containers/List'
 
 const App = () => {
     return (
-        <main className="bg-dark">
-            <div className="container">
-                <List/>
-            </div>
-        </main>
+        <Fragment>
+            <nav className="navbar navbar-dark bg-dark border-bottom border-white">
+                <a href="/" className="navbar-brand">MovieApp</a>
+            </nav>
+            <main className="bg-dark">
+                <div className="container">
+                    <List/>
+                </div>
+            </main>
+        </Fragment>
     )
 }
 
