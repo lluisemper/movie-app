de5b0f4859b862c7ad5ccd5b81b7cc5c8d09570e
lluisemper <lluisemper@gmail.com>
2020-04-21 15:22:05 +0200

    css loaded

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../components/Card/Card'
 
-const List = () => <Card/>
+const List = () => {
 
+const [data, setData] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+    const res = await fetch('../../assets/data.json')
+    const resJSON = await res.json()
+    setData(resJSON);
+}
+   fetchData(); 
+}, [])
+
+
+return (
+    <div>
+        {data.map(movie => {            
+            return <Card movie={movie} key={movie.id}/>
+        })}
+    </div>
+)
+
+}
 export default List;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import List from './containers/List'
 
 ReactDOM.render(<List/>,document.getElementById('root'));
